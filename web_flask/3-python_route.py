5fd54dc24e151421e42f2b35089298b8bf772489
Humphrey <h.nyahoja@alustudent.com>
2024-02-23 17:52:47 -0800

    correction 2 of file number 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,14 +4,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'"""
     return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_textx(text):
     """Displays 'C' followed value of text"""
@@ -32,4 +37,3 @@ def python_textx(text):
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000)
-
